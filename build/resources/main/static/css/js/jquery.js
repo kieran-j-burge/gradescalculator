6d4181ec0b09d04582e1a6cdea025ead1631722a
c1645601 <BurgeKJ@cardiff.ac.uk>
2018-07-27 12:05:44 +0100

    build

---
+++ build/resources/main/static/css/js/jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "70%";
+        // document.getElementById("flipkart-navbar").style.width = "50%";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.body.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+
+});
+
